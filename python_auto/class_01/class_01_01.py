e2540ec4774d6c9c3829d21dd26d2c4a0e39571b
liujiahai <ljhtest2017@163.com>
2020-03-09 18:10:49 +0800

    commit

---
+++ python_auto/class_01/class_01_01.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+print('Hello world!')
+
+# python package存在的意义：方便我们去组织存放代码
+# 分类组组织： 怎么分类 这些都是由你自己来决定的
+# Python 是非常佛系的代码
+# project——package——*.py
+
+# 重命名一个文件 右键-refactor_rename
+
+
